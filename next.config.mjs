73580c700558a69c78820bc655cc3df1d6d6d008
code-savan <eric.marvelboy@gmail.com>
2025-03-07 14:34:51 +0100

    added ignore eslint during build

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,12 @@
 const nextConfig = {
     images: {
         dangerouslyAllowSVG: true
-    }
+    },
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+      },
 };
 
 export default nextConfig;
