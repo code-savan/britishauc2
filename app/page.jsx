7887a48a2c9b005d35a5675a52cb14b38c33b636
code-savan <eric.marvelboy@gmail.com>
2026-04-09 12:09:10 +0100

    update

--- app/page.jsx
+++ app/page.jsx
@@ -513,7 +513,8 @@ export default function Home() {
                       ].map((item, index) => (
                         <li key={index} className="flex items-center gap-2 text-gray-700 text-sm md:text-base">
                           <svg className="w-5 h-5 text-green-500 flex-shrink-0" viewBox="0 0 20 20" fill="currentColor">
-                            <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
+                            <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                            clipRule="evenodd" />
                           </svg>
                           {item}
                         </li>
